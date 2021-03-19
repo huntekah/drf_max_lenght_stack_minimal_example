ea2809c6dfd332f98b78e38aa62cd71506875e87
huntekah <krzysztof.jurkiewicz@stxnext.com>
2021-03-19 23:45:34 +0100

    validate_name method isn't called at all

--- stack_example/user/serializers.py
+++ stack_example/user/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
-from user.models import User 
+from user.models import User
+
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
         fields = ["name"]
         extra_kwargs = {
             "name": {"required": True, "allow_blank": False, "allow_null": False},
-    }
+        }
+
+    def validate_name(self, value):
+        # I straight up raise an exception to show it doesnt even go into this validator
+        max_length = self.Meta.model.name.max_length
+        raise serializers.ValidationError(f"I am happy to see you {max_length}.")
