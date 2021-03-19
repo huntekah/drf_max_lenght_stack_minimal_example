e362801579cf5ce54f6f1b70e67f2c2556dc1731
huntekah <krzysztof.jurkiewicz@stxnext.com>
2021-03-19 23:30:50 +0100

    Experiment 1 fails

--- stack_example/user/models.py
+++ stack_example/user/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import (
-        AbstractBaseUser,
-        PermissionsMixin,
-        )
+    AbstractBaseUser,
+    PermissionsMixin,
+)
+from django.core.validators import MaxLengthValidator
 
 # Create your models here.
 
+
 class User(AbstractBaseUser, PermissionsMixin):
-    name = models.CharField("Name", max_length=42, null=True, 
-        error_messages={'max_length':"I am happy to see you {max_length}."})
+    name = models.CharField(
+        "Name",
+        max_length=42,
+        null=True,
+        validators=[MaxLengthValidator(42, "I am happy to see you {max_length}.")],
+    )
