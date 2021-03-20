c971a07b71810b2c82ffd0f1eab57d0c7d78210e
huntekah <krzysztof.jurkiewicz@stxnext.com>
2021-03-20 09:32:18 +0100

    working solution based on Tiki's answer

--- stack_example/user/serializers.py
+++ stack_example/user/serializers.py
@@ -1,11 +1,26 @@
 from rest_framework import serializers
-from user.models import User 
+from user.models import User
+from rest_framework import fields
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomCharField(fields.CharField):
+    default_error_messages = {
+        "max_length": _("I am happy to see you {max_length}."),
+    }
+
 
 class UserSerializer(serializers.ModelSerializer):
+    name = CustomCharField(
+        required=True,
+        allow_blank=False,
+        allow_null=False,
+        max_length=42,
+    )
 
     class Meta:
         model = User
         fields = ["name"]
         extra_kwargs = {
             "name": {"required": True, "allow_blank": False, "allow_null": False},
-    }
+        }
